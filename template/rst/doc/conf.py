d3b3b40dc84d512afc14d4b5ed0b8018f3712f9a
ChienYu Lin <cy20lin@gmail.com>
2018-11-30 23:05:50 +0800

    Update template rst: sphinx documentation example

--- template/rst/doc/conf.py
+++ template/rst/doc/conf.py
@@ -67,7 +67,14 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# NOTE:
+# It seems that sphinx-autobuild doesn't respect this exclude pattern.
+# It should, but it didn't. Now we have to explicitly the option
+# --ignore "*file-name-pattern-to-ignore" when using command
+# shpinx-autobuild .
+# TODO:
+# Report this issue.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/.#*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -78,7 +85,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -90,6 +98,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
--- template/rst/doc/index.rst
+++ template/rst/doc/index.rst
@@ -3,18 +3,22 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to rst's documentation!
-===============================
+Sphinx documentation template
+=============================
+
+This is the example for sphinx documentation.
+
+Sphinx is a good tool that could generate various types of
+documents using RestructuedText as it markup language.
+
+Turn plain text into your documentation.
+
+Table of contents
+-----------------
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
+   ./installation
+   ./test
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
